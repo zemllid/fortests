3a7e3d4804f86623739215aee12c83cb06a50dc5
zemllid <zemllid@gmail.com>
2025-05-31 00:39:32 +0300

    it works yay

--- main.go
+++ main.go
@@ -8,21 +8,31 @@ import (
 	"net/http"
 	"os"
 	"time"
+	"strconv"
 
 	_ "github.com/lib/pq" // драйвер PostgreSQL
 )
 
 func main() {
 	// Чтение настроек подключения из переменных окружения
-	dbHost := getEnv("DB_HOST", "localhost")
+	dbHost := getEnv("DB_HOST", "postgresf")
 	dbPort := getEnv("DB_PORT", "5432")
 	dbUser := getEnv("DB_USER", "postgres")
 	dbPassword := getEnv("DB_PASSWORD", "postgres")
 	dbName := getEnv("DB_NAME", "frogs_db")
+	
+	delaySec, errD:= strconv.ParseInt(getEnv("DB_DELAY", "5"), 10, 64)
+if errD != nil {
+    panic(errD)
+}
+	log.Printf("wait for %v sec", delaySec)
+	time.Sleep(time.Duration(delaySec) * time.Second)
 
 	// Формирование строки подключения
 	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
 		dbHost, dbPort, dbUser, dbPassword, dbName)
+		
+    log.Printf("test connect %v",psqlInfo)
 
 	// Подключение к базе данных PostgreSQL
 	db, err := sql.Open("postgres", psqlInfo)
@@ -31,11 +41,13 @@ func main() {
 	}
 	defer db.Close()
 
+    log.Printf("success connect %v",psqlInfo)
+
 	// Проверка соединения с БД
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 	if err = db.PingContext(ctx); err != nil {
-		log.Fatalf("Ошибка ping БД: %v", err)
+		log.Fatalf("Ошибка ping БД: %v ", err)
 	}
 	log.Println("Подключение к БД успешно установлено")
 
--- static/script.js
+++ static/script.js
@@ -1,3 +1,5 @@
+document.addEventListener("DOMContentLoaded", async () => {await loadFrogs();})
+
 async function createMessage() {
     var inputID = document.getElementById("inputName").value;
     var inputa = document.getElementById("inputSpecies").value;
@@ -11,21 +13,37 @@ async function createMessage() {
       });
 }
 
-async function getMessage() {
+async function loadFrogs() {
     let response = await fetch('/frogs');  
     if (response.ok){
         let json = await response.json()
-        let element = document.getElementById("table");
-        element.innerText = ""
+        let element = document.getElementById("frogsTableBody");
+		
+        let plain = document.getElementById("plain");
+        element.innerHtml = ""
+		plain.innerHtml = ""
+		if (json==null) {
+			plain.innerHTML= "Жаб нет :("
+		} else {
         console.log(json.length)
+		let a=""
+		let b=""
         for(var i=0;i<json.length; i++) {
-            element.innerText = element.innerText + "\n" + json[i].name + "  " 
+            b+= "<td> <tr>" + json[i].id + "<tr>"
+			+ json[i].name + "<tr>" 
+                                                         + json[i].species + "<tr>" 
+                                                         + json[i].habitat + "<tr>"  
+                                                         + json[i].age
+			a += "\n <br>" + json[i].id + " "
+			+ json[i].name + "  " 
                                                          + json[i].species + "  " 
                                                          + json[i].habitat + "  "  
-                                                         + json[i].age
+                                                         + json[i].age											 
             console.log(json[i].name + "  " +  json[i].species + "  " + json[i].habitat)
         };
-
+		plain.innerHTML=a
+		element.innerHtml = b
+		}
     } else{
         alert("Error: " + response.json())
     }  
